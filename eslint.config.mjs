bd923ef3aa924cc18c3f8ff4a815f53ea687991c
bd_woujoud <wwwwboudhina@gmail.com>
2025-09-17 03:30:04 +0100

    register

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,9 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "react/no-unescaped-entities": "off", // <-- désactive la vérification des apostrophes
+    },
   },
 ];
 
